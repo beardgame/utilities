f3c879da4cfafa09c7f0fc20bda53de226cdc63b
Tom <tom@tomrijnbeek.nl>
2015-02-20 16:16:41 +0100

    Added arc classes.

---
+++ src/Math/Geometry/Arc2.cs
@@ -0,0 +1,22 @@
+﻿using OpenTK;
+
+namespace Bearded.Utilities.Math.Geometry
+{
+    /// <summary>
+    /// Represents an arc in two-dimensional Euclidean space.
+    /// </summary>
+    public abstract class Arc2 : Arc<Vector2>
+    {
+        /// <summary>
+        /// Initializes the arc.
+        /// </summary>
+        /// <param name="segments">The amount of linear segments the arc is split in. A larger amount of segments results in higher precision for length and remapping.</param>
+        protected Arc2(int segments = 100)
+            : base(segments) { }
+
+        protected override float getDistanceBetween(Vector2 p1, Vector2 p2)
+        {
+            return (p2 - p1).Length;
+        }
+    }
+}
---
+++ src/Math/Geometry/Arc3.cs
@@ -0,0 +1,22 @@
+﻿using OpenTK;
+
+namespace Bearded.Utilities.Math.Geometry
+{
+    /// <summary>
+    /// Represents an arc in three-dimensional Euclidean space.
+    /// </summary>
+    public abstract class Arc3 : Arc<Vector3>
+    {
+        /// <summary>
+        /// Initializes the arc.
+        /// </summary>
+        /// <param name="segments">The amount of linear segments the arc is split in. A larger amount of segments results in higher precision for length and remapping.</param>
+        protected Arc3(int segments = 100)
+            : base(segments) { }
+
+        protected override float getDistanceBetween(Vector3 p1, Vector3 p2)
+        {
+            return (p2 - p1).Length;
+        }
+    }
+}
