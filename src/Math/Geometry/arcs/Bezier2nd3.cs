f6bd428e0ce8b5fc1b70c78dd6c100dbc6f2ca56
Tom <tom@tomrijnbeek.nl>
2015-02-20 16:26:48 +0100

    Added arc logic.

---
+++ src/Math/Geometry/arcs/Bezier2nd3.cs
@@ -0,0 +1,35 @@
+﻿using OpenTK;
+
+namespace Bearded.Utilities.Math.Geometry
+{
+    /// <summary>
+    /// Represents a quadratic Bezier curve in three-dimensional space.
+    /// </summary>
+    // ReSharper disable once InconsistentNaming
+    public sealed class Bezier2nd3 : Arc3
+    {
+        private readonly Vector3 p0;
+        private readonly Vector3 p1;
+        private readonly Vector3 p2;
+
+        /// <summary>
+        /// Initializes the Bezier curve.
+        /// </summary>
+        /// <param name="p0"></param>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        /// <param name="segments">The amount of linear segments the arc is split in. A larger amount of segments results in higher precision for length and remapping.</param>
+        public Bezier2nd3(Vector3 p0, Vector3 p1, Vector3 p2, int segments = 100)
+            : base(segments)
+        {
+            this.p0 = p0;
+            this.p1 = p1;
+            this.p2 = p2;
+        }
+
+        protected override Vector3 getPointAt(float t)
+        {
+            return Interpolate.Bezier(p0, p1, p2, t);
+        }
+    }
+}
---
+++ src/Math/Geometry/arcs/Bezier3rd3.cs
@@ -0,0 +1,38 @@
+﻿using OpenTK;
+
+namespace Bearded.Utilities.Math.Geometry
+{
+    /// <summary>
+    /// Represents a cubic Bezier curve in three-dimensional space.
+    /// </summary>
+    // ReSharper disable once InconsistentNaming
+    public sealed class Bezier3rd3 : Arc3
+    {
+        private readonly Vector3 p0;
+        private readonly Vector3 p1;
+        private readonly Vector3 p2;
+        private readonly Vector3 p3;
+
+        /// <summary>
+        /// Initializes the Bezier curve.
+        /// </summary>
+        /// <param name="p0"></param>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        /// <param name="p3"></param>
+        /// <param name="segments">The amount of linear segments the arc is split in. A larger amount of segments results in higher precision for length and remapping.</param>
+        public Bezier3rd3(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, int segments = 100)
+            : base(segments)
+        {
+            this.p0 = p0;
+            this.p1 = p1;
+            this.p2 = p2;
+            this.p3 = p3;
+        }
+
+        protected override Vector3 getPointAt(float t)
+        {
+            return Interpolate.Bezier(p0, p1, p2, p3, t);
+        }
+    }
+}
