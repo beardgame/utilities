09b467fc7c930e060217118ed8400c01c3623f1e
amulware <amul@amulware.net>
2015-09-14 01:15:27 +0200

    added basic rectangular tilemap, tile, and direction types, as well as some tests

---
+++ src/Tilemaps/Step.cs
@@ -0,0 +1,17 @@
+﻿namespace Bearded.Utilities.Tilemaps
+{
+    /// <summary>
+    /// Represents a step taken in a Tilemap from one tile to another.
+    /// </summary>
+    struct Step
+    {
+        public readonly sbyte X;
+        public readonly sbyte Y;
+
+        public Step(sbyte x, sbyte y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
