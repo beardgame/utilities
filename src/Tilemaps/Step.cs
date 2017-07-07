330528dbaff8d9343bf289fee13a6f29c04ba9f7
amulware <amul@amulware.net>
2017-07-07 22:29:42 +0200

    :lipstick:

--- src/Tilemaps/Step.cs
+++ src/Tilemaps/Step.cs
@@ -1,17 +1,14 @@
 ﻿namespace Bearded.Utilities.Tilemaps
 {
-    /// <summary>
-    /// Represents a step taken in a Tilemap from one tile to another.
-    /// </summary>
-    struct Step
+    internal struct Step
     {
         public readonly sbyte X;
         public readonly sbyte Y;
 
         public Step(sbyte x, sbyte y)
         {
-            this.X = x;
-            this.Y = y;
+            X = x;
+            Y = y;
         }
     }
 }
