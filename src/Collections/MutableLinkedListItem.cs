cc86c7a26df67a6bc4d81a9f69aec64fdfeefbb8
amulware <amul@amulware.net>
2017-07-07 23:08:29 +0200

    :unlock: Make MutableLinkedListItem public

--- src/Collections/MutableLinkedListItem.cs
+++ src/Collections/MutableLinkedListItem.cs
@@ -9,25 +9,8 @@ namespace Bearded.Utilities.Collections
     /// Make sure you add this as node and not as item to the linked list to prevent creation of the node wrapper.
     /// </summary>
     /// <typeparam name="TMe">The type of the inheriting class.</typeparam>
-    abstract class MutableLinkedListItem<TMe> : MutableLinkedListNode<TMe>
+    public abstract class MutableLinkedListItem<TMe> : MutableLinkedListNode<TMe>
         where TMe : MutableLinkedListNode<TMe>
-    {
-
-    }
-
-    // ReSharper disable once UnusedTypeParameter
-    /// <summary>
-    /// This type can be used to have a class be both the value as well as the node in a linked list, mixed with actual nodes.
-    /// Usage:
-    /// MyClass : MutableLinkedListItem&lt;MyClass, MyInterface&gt;
-    /// MutableLinkedList&lt;MyInterface&gt;
-    /// Make sure you add this as node and not as item to the linked list to prevent creation of the node wrapper.
-    /// </summary>
-    /// <typeparam name="TMe">The type of the inheriting class.</typeparam>
-    /// <typeparam name="TInterface">The type of the lists interface.</typeparam>
-    abstract class MutableLinkedListItem<TMe, TInterface> : MutableLinkedListNode<TInterface>
-        where TInterface : class
-        where TMe : TInterface
     {
 
     }
