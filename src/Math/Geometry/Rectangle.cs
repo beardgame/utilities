59b3d811a5becfaaf771c8ebeabfde2c992b79a7
amulware <amul@amulware.net>
2017-07-07 22:48:16 +0200

    :racehorse: Rectangle : IEquatable

--- src/Math/Geometry/Rectangle.cs
+++ src/Math/Geometry/Rectangle.cs
@@ -8,7 +8,7 @@ namespace Bearded.Utilities.Math.Geometry
     /// All properties assume the x axis pointing right and the y axis pointing down.
     /// Negative width or height rectangles are not allowed, and the constructor will throw with those values.
     /// </summary>
-    public struct Rectangle
+    public struct Rectangle : IEquatable<Rectangle>
     {
         public float Left { get; }
         public float Top { get; }
@@ -59,6 +59,27 @@ namespace Bearded.Utilities.Math.Geometry
 
         public static Rectangle WithSides(float top, float right, float bottom, float left)
             => new Rectangle(top, left, bottom - top, right - left);
-        
+
+        public bool Equals(Rectangle other)
+            // ReSharper disable CompareOfFloatsByEqualityOperator
+            => Left == other.Left && Right == other.Right && Width == other.Width && Height == other.Height;
+            // ReSharper restore CompareOfFloatsByEqualityOperator
+
+        public override bool Equals(object obj) => obj is Rectangle && Equals((Rectangle) obj);
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Left.GetHashCode();
+                hashCode = (hashCode * 397) ^ Top.GetHashCode();
+                hashCode = (hashCode * 397) ^ Width.GetHashCode();
+                hashCode = (hashCode * 397) ^ Height.GetHashCode();
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(Rectangle left, Rectangle right) => left.Equals(right);
+        public static bool operator !=(Rectangle left, Rectangle right) => !left.Equals(right);
     }
 }
