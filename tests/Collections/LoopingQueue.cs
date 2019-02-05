56a19d1a990422ee68598454d191409cc7e7d03d
Paul Scharf <amul@amulware.net>
2019-02-05 21:09:06 +0000

    ⚠ Make DeletableObjectList safer (#137)
    * :warning: Make deletable object list safer
    
    * :lipstick:
    
    * :lipstick:
    
    * :abc: Reorder members
    
    * :white_check_mark: Add tests
    
    * 💄
    
    * ❓ Remove randomness from tests

---
+++ tests/Collections/LoopingQueue.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+namespace Bearded.Utilities.Tests.Collections
+{
+    public static class LoopingQueue
+    {
+        public static LoopingQueue<T> Looping<T>(this IEnumerable<T> items) => new LoopingQueue<T>(items);
+    }
+
+    public class LoopingQueue<T>
+    {
+        private readonly Queue<T> queue;
+
+        public LoopingQueue(IEnumerable<T> items)
+        {
+            queue = new Queue<T>(items);
+        }
+
+        public T Next()
+        {
+            var item = queue.Dequeue();
+            queue.Enqueue(item);
+            return item;
+        }
+    }
+}
