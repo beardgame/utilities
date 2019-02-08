174c9af6e3e330594002b26f3d6cbd94cd91d21f
Tom <tom@tomrijnbeek.nl>
2019-02-08 21:13:22 +0000

    :white_check_mark: Increase test coverage

--- tests/Graphs/DirectedAcyclicGraphTransitiveReducerTests.cs
+++ tests/Graphs/DirectedAcyclicGraphTransitiveReducerTests.cs
@@ -9,6 +9,11 @@ namespace Bearded.Utilities.Tests.Graphs
         [Fact]
         public void GetTransitiveReduction_DoesNotRemoveElements()
         {
+            /*
+             * ----------|
+             * |         V
+             * 1 -> 2 -> 3
+             */
             var graph = DirectedGraphBuilder<string>.NewBuilder()
                 .AddElement("one")
                 .AddElement("two")
@@ -18,6 +23,9 @@ namespace Bearded.Utilities.Tests.Graphs
                 .AddArrow("one", "three")
                 .CreateAcyclicGraphUnsafe();
 
+            /*
+             * 1 -> 2 -> 3
+             */
             var reducedGraph = DirectedAcyclicGraphTransitiveReducer.ReduceGraph(graph);
 
             reducedGraph.Elements.Should().Contain(graph.Elements);
@@ -26,6 +34,11 @@ namespace Bearded.Utilities.Tests.Graphs
         [Fact]
         public void GetTransitiveReduction_RemovesTransitiveEdge()
         {
+            /*
+             * ----------|
+             * |         V
+             * 1 -> 2 -> 3
+             */
             var graph = DirectedGraphBuilder<string>.NewBuilder()
                 .AddElement("one")
                 .AddElement("two")
@@ -35,6 +48,9 @@ namespace Bearded.Utilities.Tests.Graphs
                 .AddArrow("one", "three")
                 .CreateAcyclicGraphUnsafe();
 
+            /*
+             * 1 -> 2 -> 3
+             */
             var reducedGraph = DirectedAcyclicGraphTransitiveReducer.ReduceGraph(graph);
 
             reducedGraph.GetDirectSuccessorsOf("one").Should().NotContain("three");
@@ -44,6 +60,9 @@ namespace Bearded.Utilities.Tests.Graphs
         [Fact]
         public void GetTransitiveReduction_KeepsRequiredEdges()
         {
+            /*
+             * 1 -> 2 -> 3
+             */
             var graph = DirectedGraphBuilder<string>.NewBuilder()
                 .AddElement("one")
                 .AddElement("two")
@@ -53,12 +72,50 @@ namespace Bearded.Utilities.Tests.Graphs
                 .AddArrow("one", "three")
                 .CreateAcyclicGraphUnsafe();
 
+            /*
+             * 1 -> 2 -> 3
+             */
             var reducedGraph = DirectedAcyclicGraphTransitiveReducer.ReduceGraph(graph);
 
             reducedGraph.GetDirectSuccessorsOf("one").Should().Contain("two");
             reducedGraph.GetDirectPredecessorsOf("two").Should().Contain("one");
             reducedGraph.GetDirectSuccessorsOf("two").Should().Contain("three");
             reducedGraph.GetDirectPredecessorsOf("three").Should().Contain("two");
+        }
+
+        [Fact]
+        public void GetTransitiveReduction_HandlesCasesWithMultiplePaths()
+        {
+            /*
+             * ----------|
+             * |         V
+             * 1 -> 2 -> 3 -> 4
+             *      |         ^
+             *      ----------|
+             */
+            var graph = DirectedGraphBuilder<string>.NewBuilder()
+                .AddElement("one")
+                .AddElement("two")
+                .AddElement("three")
+                .AddElement("four")
+                .AddArrow("one", "two")
+                .AddArrow("two", "three")
+                .AddArrow("three", "four")
+                .AddArrow("one", "three")
+                .AddArrow("two", "four")
+                .CreateAcyclicGraphUnsafe();
+            
+            /*
+             * 1 -> 2 -> 3 -> 4
+             */
+            var reducedGraph = DirectedAcyclicGraphTransitiveReducer.ReduceGraph(graph);
+
+            reducedGraph.GetDirectSuccessorsOf("one").Should().Contain("two");
+            reducedGraph.GetDirectPredecessorsOf("two").Should().Contain("one");
+            reducedGraph.GetDirectSuccessorsOf("two").Should().Contain("three");
+            reducedGraph.GetDirectPredecessorsOf("three").Should().Contain("two");
+            reducedGraph.GetDirectSuccessorsOf("three").Should().Contain("four");
+            reducedGraph.GetDirectPredecessorsOf("four").Should().Contain("three");
         }
     }
 }
