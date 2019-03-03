c6d277c8a608fb180f8bfb5c1475ea309dacd5ac
Tom <tom@tomrijnbeek.nl>
2019-03-03 12:17:23 +0000

    :rotating_light: Update tests to nested class style

--- Bearded.Utilities.Tests/Core/MaybeTests.cs
+++ Bearded.Utilities.Tests/Core/MaybeTests.cs
@@ -1,119 +1,137 @@
+using System.Diagnostics.CodeAnalysis;
 using FluentAssertions;
 using Xunit;
 using Xunit.Sdk;
 
 namespace Bearded.Utilities.Tests
 {
-    public class MaybeTests
+    public sealed class MaybeTests
     {
-        [Fact]
-        public void ValueOrDefault_ReturnsDefaultOnNothing()
+        public sealed class ValueOrDefault
         {
-            var maybe = Maybe.Nothing<int>();
+            [Fact]
+            public void ReturnsDefaultOnNothing()
+            {
+                var maybe = Maybe.Nothing<int>();
 
-            maybe.ValueOrDefault(100).Should().Be(100);
+                maybe.ValueOrDefault(100).Should().Be(100);
+            }
+
+            [Fact]
+            public void ReturnsValueOnJust()
+            {
+                var maybe = Maybe.Just(200);
+
+                maybe.ValueOrDefault(100).Should().Be(200);
+            }
         }
 
-        [Fact]
-        public void ValueOrDefault_ReturnsValueOnJust()
+        public sealed class Select
         {
-            var maybe = Maybe.Just(200);
+            [Fact]
+            public void MapsNothingToNothing()
+            {
+                var maybe = Maybe.Nothing<int>();
 
-            maybe.ValueOrDefault(100).Should().Be(200);
+                maybe.Select(i => i * 2).Should().Be(Maybe.Nothing<int>());
+            }
+
+            [Fact]
+            public void MapsValueToJust()
+            {
+                var maybe = Maybe.Just(100);
+
+                maybe.Select(i => i * 2).Should().Be(Maybe.Just(200));
+            }
         }
 
-        [Fact]
-        public void Select_MapsNothingToNothing()
+        public sealed class SelectMany
         {
-            var maybe = Maybe.Nothing<int>();
+            [Fact]
+            public void MapsNothingToNothing()
+            {
+                var maybe = Maybe.Nothing<int>();
 
-            maybe.Select(i => i * 2).Should().Be(Maybe.Nothing<int>());
+                maybe.SelectMany(i => Maybe.Just(i * 2)).Should().Be(Maybe.Nothing<int>());
+            }
+
+            [Fact]
+            public void MapsValueToJust()
+            {
+                var maybe = Maybe.Just(100);
+
+                maybe.SelectMany(i => Maybe.Just(i * 2)).Should().Be(Maybe.Just(200));
+            }
+
+            [Fact]
+            public void MapsValueToNothing()
+            {
+                var maybe = Maybe.Just(100);
+
+                maybe.SelectMany(i => Maybe.Nothing<int>()).Should().Be(Maybe.Nothing<int>());
+            }
         }
 
-        [Fact]
-        public void Select_MapsValueToJust()
+        public sealed class Match
         {
-            var maybe = Maybe.Just(100);
+            [Fact]
+            [SuppressMessage("ReSharper", "ArgumentsStyleAnonymousFunction")]
+            public void CallsOnNothingOnNothing()
+            {
+                var maybe = Maybe.Nothing<int>();
 
-            maybe.Select(i => i * 2).Should().Be(Maybe.Just(200));
-        }
+                var isCalled = false;
+                maybe.Match(
+                    onValue: (val) => throw new XunitException("Wrong method called"),
+                    onNothing: () => isCalled = true);
 
-        [Fact]
-        public void SelectMany_MapsNothingToNothing()
-        {
-            var maybe = Maybe.Nothing<int>();
-
-            maybe.SelectMany(i => Maybe.Just(i * 2)).Should().Be(Maybe.Nothing<int>());
-        }
-
-        [Fact]
-        public void SelectMany_MapsValueToJust()
-        {
-            var maybe = Maybe.Just(100);
-
-            maybe.SelectMany(i => Maybe.Just(i * 2)).Should().Be(Maybe.Just(200));
-        }
-
-        [Fact]
-        public void SelectMany_MapsValueToNothing()
-        {
-            var maybe = Maybe.Just(100);
-
-            maybe.SelectMany(i => Maybe.Nothing<int>()).Should().Be(Maybe.Nothing<int>());
-        }
-
-        [Fact]
-        public void Match_CallsOnNothingOnNothing()
-        {
-            var maybe = Maybe.Nothing<int>();
-
-            var isCalled = false;
-            maybe.Match(
-                onValue: (val) => throw new XunitException("Wrong method called"),
-                onNothing: () => isCalled = true);
-
-            isCalled.Should().BeTrue("onNothing should have been called");
-        }
+                isCalled.Should().BeTrue("onNothing should have been called");
+            }
         
-        [Fact]
-        public void Match_CallsOnValueWithValueOnJust()
-        {
-            var maybe = Maybe.Just(100);
+            [Fact]
+            [SuppressMessage("ReSharper", "ArgumentsStyleAnonymousFunction")]
+            public void CallsOnValueWithValueOnJust()
+            {
+                var maybe = Maybe.Just(100);
 
-            var isCalled = false;
-            maybe.Match(
-                onValue: (val) =>
-                {
-                    val.Should().Be(100);
-                    isCalled = true;
-                },
-                onNothing: () => throw new XunitException("Wrong method called"));
+                var isCalled = false;
+                maybe.Match(
+                    onValue: (val) =>
+                    {
+                        val.Should().Be(100);
+                        isCalled = true;
+                    },
+                    onNothing: () => throw new XunitException("Wrong method called"));
             
-            isCalled.Should().BeTrue("onValue should have been called");
+                isCalled.Should().BeTrue("onValue should have been called");
+            }
         }
 
-        [Fact]
-        public void FromNullable_OnReferenceTypeNull_ReturnsNothing()
+        public sealed class FromNullable
         {
-            Maybe.FromNullable((string) null).Should().Be(Maybe.Nothing<string>());
-        }
+            [Fact]
+            public void ReturnsNothingOnReferenceTypeNull()
+            {
+                Maybe.FromNullable((string) null).Should().Be(Maybe.Nothing<string>());
+            }
 
-        [Fact]
-        public void FromNullable_OnReferenceTypeSet_ReturnsJust()
-        {
-            Maybe.FromNullable("the game").Should().Be(Maybe.Just("the game"));
-        }
+            [Fact]
+            public void ReturnsJustOnReferenceTypeSet()
+            {
+                Maybe.FromNullable("the game").Should().Be(Maybe.Just("the game"));
+            }
         
-        [Fact]
-        public void FromNullable_OnNullableNoValue_ReturnsNothing()
-        {
-            Maybe.FromNullable((int?) null).Should().Be(Maybe.Nothing<int>());
-        }
+            [Fact]
+            public void ReturnsNothingOnNullableNoValue()
+            {
+                Maybe.FromNullable((int?) null).Should().Be(Maybe.Nothing<int>());
+            }
         
-        [Fact]
-        public void FromNullable_OnNullableWithValue_ReturnsJust()
-        {
-            Maybe.FromNullable((int?) 10).Should().Be(Maybe.Just(10));
+            [Fact]
+            public void ReturnsJustOnNullableWithValue()
+            {
+                Maybe.FromNullable((int?) 10).Should().Be(Maybe.Just(10));
+            }
         }
     }
 }
