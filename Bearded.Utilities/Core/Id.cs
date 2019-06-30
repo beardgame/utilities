4f6b375b9e76d1aca77d013c531275f3e1bdd987
Tom Rijnbeek <tom@tomrijnbeek.nl>
2019-06-30 12:39:48 +0100

    :sparkles: Add descriptive ToString to Id
    Closes #162

--- Bearded.Utilities/Core/Id.cs
+++ Bearded.Utilities/Core/Id.cs
@@ -16,9 +16,10 @@ namespace Bearded.Utilities
         public bool IsValid => Value != 0;
         public static Id<T> Invalid => new Id<T>(0);
 
+        public override string ToString() => $"{typeof(T).Name}:{Value}";
         public override int GetHashCode() => Value.GetHashCode();
         public bool Equals(Id<T> other) => Value.Equals(other.Value);
-        public override bool Equals(object obj) => obj is Id<T> && Equals((Id<T>)obj);
+        public override bool Equals(object obj) => obj is Id<T> id && Equals(id);
         public static bool operator ==(Id<T> left, Id<T> right) => left.Equals(right);
         public static bool operator !=(Id<T> left, Id<T> right) => !(left == right);
     }
