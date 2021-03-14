587da709359fc5d4ec69d607009d3189f9b63c72
Tom Rijnbeek <tom@tomrijnbeek.me>
2021-03-14 15:07:46 +0000

    🚚 Extract interpolation interfaces to their own files

--- Bearded.Utilities/Core/interpolation/Interpolation1.cs
+++ Bearded.Utilities/Core/interpolation/Interpolation1.cs
@@ -48,9 +48,4 @@ namespace Bearded.Utilities
             }
         }
     }
-
-    public interface IInterpolationMethod1
-    {
-        public double Interpolate(double from, double to, double t);
-    }
 }
--- Bearded.Utilities/Core/interpolation/Interpolation2.cs
+++ Bearded.Utilities/Core/interpolation/Interpolation2.cs
@@ -23,9 +23,4 @@ namespace Bearded.Utilities
             }
         }
     }
-
-    public interface IInterpolationMethod2
-    {
-        public double Interpolate(double value00, double value10, double value01, double value11, double u, double v);
-    }
 }
