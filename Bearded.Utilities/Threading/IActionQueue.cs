e4d196ab5867e637a5c6c8c90b7dc454929f22ff
amulware <amul@amulware.net>
2021-06-08 22:09:09 +0100

    🧵 Make IActionQueue async

--- Bearded.Utilities/Threading/IActionQueue.cs
+++ Bearded.Utilities/Threading/IActionQueue.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 
 namespace Bearded.Utilities.Threading
 {
@@ -11,52 +12,18 @@ namespace Bearded.Utilities.Threading
         /// Queues an action to run. Returns immediately.
         /// </summary>
         /// <param name="action">The action to run.</param>
-        void RunAndForget(Action action);
+        void Queue(Action action);
 
         /// <summary>
-        /// Queues an action to run. Returns only after the action has been executed.
+        /// Queues an action to run. Returns a task that completes when the action has been executed.
         /// </summary>
         /// <param name="action">The action to run.</param>
-        void RunAndAwait(Action action);
+        Task Run(Action action);
 
         /// <summary>
-        /// Queues a parameterless function to run. Returns the return value of the function only after the function has been executed.
+        /// Queues a parameterless function to run. Returns a task that completes when the function has been executed.
         /// </summary>
-        /// <param name="action">The function to run.</param>
-        T RunAndReturn<T>(Func<T> action);
-
-        /// <summary>
-        /// Queues a function with one parameter to run. Returns the return value of the function only after the function has been executed.
-        /// </summary>
-        /// <param name="action">The function to run.</param>
-        /// <param name="p0">The argument for calling the function.</param>
-        T RunAndReturn<TP0, T>(Func<TP0, T> action, TP0 p0);
-
-        /// <summary>
-        /// Queues a function with two parameters to run. Returns the return value of the function only after the function has been executed.
-        /// </summary>
-        /// <param name="action">The function to run.</param>
-        /// <param name="p0">The first argument for calling the function.</param>
-        /// <param name="p1">The second argument for calling the function.</param>
-        T RunAndReturn<TP0, TP1, T>(Func<TP0, TP1, T> action, TP0 p0, TP1 p1);
-
-        /// <summary>
-        /// Queues a function with three parameters to run. Returns the return value of the function only after the function has been executed.
-        /// </summary>
-        /// <param name="action">The function to run.</param>
-        /// <param name="p0">The first argument for calling the function.</param>
-        /// <param name="p1">The second argument for calling the function.</param>
-        /// <param name="p2">The third argument for calling the function.</param>
-        T RunAndReturn<TP0, TP1, TP2, T>(Func<TP0, TP1, TP2, T> action, TP0 p0, TP1 p1, TP2 p2);
-
-        /// <summary>
-        /// Queues a function with four parameters to run. Returns the return value of the function only after the function has been executed.
-        /// </summary>
-        /// <param name="action">The function to run.</param>
-        /// <param name="p0">The first argument for calling the function.</param>
-        /// <param name="p1">The second argument for calling the function.</param>
-        /// <param name="p2">The third argument for calling the function.</param>
-        /// <param name="p3">The fourth argument for calling the function.</param>
-        T RunAndReturn<TP0, TP1, TP2, TP3, T>(Func<TP0, TP1, TP2, TP3, T> action, TP0 p0, TP1 p1, TP2 p2, TP3 p3);
+        /// <param name="function">The function to run.</param>
+        Task<T> Run<T>(Func<T> function);
     }
 }
