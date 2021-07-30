4ac81d7b444eea7b83ccba0f565751b48a350b7a
Tom Rijnbeek <tom@tomrijnbeek.me>
2021-07-30 18:54:58 +0100

    ✅ Add property tests for Angle, StartPoint, EndPoint

--- Bearded.Utilities.Tests/Generators/FloatGenerators.cs
+++ Bearded.Utilities.Tests/Generators/FloatGenerators.cs
@@ -1,4 +1,5 @@
-﻿using FsCheck;
+﻿using System;
+using FsCheck;
 
 namespace Bearded.Utilities.Tests.Generators
 {
@@ -8,6 +9,15 @@ namespace Bearded.Utilities.Tests.Generators
         {
             public static Arbitrary<float> Floats()
                 => Arb.Default.Float32().Filter(f => !float.IsInfinity(f) && !float.IsNaN(f));
+        }
+
+        public static class PositiveNonInfiniteNonNaN
+        {
+            public static Arbitrary<float> Floats()
+                => NonInfiniteNonNaN.Floats().Generator
+                    .Where(f => f != 0)
+                    .Select(Math.Abs)
+                    .ToArbitrary();
         }
     }
 }
--- Bearded.Utilities.Tests/Geometry/CircularArc2Tests.cs
+++ Bearded.Utilities.Tests/Geometry/CircularArc2Tests.cs
@@ -42,6 +42,8 @@ namespace Bearded.Utilities.Tests.Geometry
             [Property(Arbitrary = new [] { typeof(DirectionGenerators.All) })]
             public void ReturnsReversedShortArcIfArcIsShort(Direction2 from, Direction2 to)
             {
+                if (from == to) return;
+
                 var arc = CircularArc2.ShortArcBetweenDirections(Vector2.Zero, 1, from, to);
 
                 var actual = arc.Reversed;
@@ -53,12 +55,65 @@ namespace Bearded.Utilities.Tests.Geometry
             [Property(Arbitrary = new [] { typeof(DirectionGenerators.All) })]
             public void ReturnsReversedShortArcIfArcIsLong(Direction2 from, Direction2 to)
             {
+                if (from == to) return;
+
                 var arc = CircularArc2.LongArcBetweenDirections(Vector2.Zero, 1, from, to);
 
                 var actual = arc.Reversed;
 
                 var expected = CircularArc2.LongArcBetweenDirections(Vector2.Zero, 1, to, from);
                 actual.Should().Be(expected);
+            }
+        }
+
+        public sealed class Angle
+        {
+            [Property(Arbitrary = new[] {typeof(DirectionGenerators.All)})]
+            public void IsSmallerThanPiRadForShortArcs(Direction2 from, Direction2 to)
+            {
+                if (from == to) return;
+
+                var arc = CircularArc2.ShortArcBetweenDirections(Vector2.Zero, 1, from, to);
+
+                arc.Angle.MagnitudeInRadians.Should().BeInRange(0, MathConstants.Pi);
+            }
+
+            [Property(Arbitrary = new[] {typeof(DirectionGenerators.All)})]
+            public void IsLargerThanPiRadForShortArcs(Direction2 from, Direction2 to)
+            {
+                if (from == to) return;
+
+                var arc = CircularArc2.LongArcBetweenDirections(Vector2.Zero, 1, from, to);
+
+                arc.Angle.MagnitudeInRadians.Should().BeInRange(MathConstants.Pi, MathConstants.TwoPi);
+            }
+        }
+
+        public sealed class StartPoint
+        {
+            [Property(Arbitrary = new[]
+                {typeof(DirectionGenerators.All), typeof(FloatGenerators.PositiveNonInfiniteNonNaN)})]
+            public void ReturnsPointOnArc(Direction2 from, Direction2 to, float radius)
+            {
+                if (from == to) return;
+
+                var arc = CircularArc2.ShortArcBetweenDirections(Vector2.Zero, radius, from, to);
+
+                arc.StartPoint.LengthSquared.Should().BeApproximately(radius * radius, 0.1f);
+            }
+        }
+
+        public sealed class EndPoint
+        {
+            [Property(Arbitrary = new[]
+                {typeof(DirectionGenerators.All), typeof(FloatGenerators.PositiveNonInfiniteNonNaN)})]
+            public void ReturnsPointOnArc(Direction2 from, Direction2 to, float radius)
+            {
+                if (from == to) return;
+
+                var arc = CircularArc2.ShortArcBetweenDirections(Vector2.Zero, radius, from, to);
+
+                arc.EndPoint.LengthSquared.Should().BeApproximately(radius * radius, 0.1f);
             }
         }
     }
