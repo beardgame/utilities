d0ec5151563fbb7222a9c4b1ac4b5e994e86cc63
Tom Rijnbeek <tom@tomrijnbeek.me>
2021-11-05 21:12:07 +0000

    ✅ Use base class for noise tests

--- Bearded.Utilities.Tests/Noise/PerlinNoiseTests.cs
+++ Bearded.Utilities.Tests/Noise/PerlinNoiseTests.cs
@@ -1,64 +1,9 @@
-using System;
 using Bearded.Utilities.Noise;
-using Bearded.Utilities.Tests.Generators;
-using FluentAssertions;
-using FsCheck.Xunit;
 
 namespace Bearded.Utilities.Tests.Noise
 {
-    public sealed class PerlinNoiseTests
+    public sealed class PerlinNoiseTests : NoiseTests
     {
-        [Property(Arbitrary = new[] { typeof(DoubleGenerators.UnitIntervalUpperBoundExclusive) })]
-        public void GeneratesMapThatThrowsIfXCoordinateTooSmall(int seed, double x, double y)
-        {
-            var map = PerlinNoise.Generate(5, 5, seed);
-
-            Func<double> action = () => map.ValueAt(x - 1.0, y);
-            action.Should().Throw<ArgumentOutOfRangeException>();
-        }
-
-        [Property(Arbitrary = new[] { typeof(DoubleGenerators.UnitIntervalUpperBoundExclusive) })]
-        public void GeneratesMapThatThrowsIfXCoordinateTooBig(int seed, double x, double y)
-        {
-            var map = PerlinNoise.Generate(5, 5, seed);
-
-            Func<double> action = () => map.ValueAt(x + 1.0, y);
-            action.Should().Throw<ArgumentOutOfRangeException>();
-        }
-
-        [Property(Arbitrary = new[] { typeof(DoubleGenerators.UnitIntervalUpperBoundExclusive) })]
-        public void GeneratesMapThatThrowsIfYCoordinateTooSmall(int seed, double x, double y)
-        {
-            var map = PerlinNoise.Generate(5, 5, seed);
-
-            Func<double> action = () => map.ValueAt(x, y - 1.0);
-            action.Should().Throw<ArgumentOutOfRangeException>();
-        }
-
-        [Property(Arbitrary = new[] { typeof(DoubleGenerators.UnitIntervalUpperBoundExclusive) })]
-        public void GeneratesMapThatThrowsIfYCoordinateTooBig(int seed, double x, double y)
-        {
-            var map = PerlinNoise.Generate(5, 5, seed);
-
-            Func<double> action = () => map.ValueAt(x, y + 1.0);
-            action.Should().Throw<ArgumentOutOfRangeException>();
-        }
-
-        [Property(Arbitrary = new[] { typeof(DoubleGenerators.UnitIntervalUpperBoundExclusive) })]
-        public void GeneratesMapWithValuesInUnitInterval(int seed, double x, double y)
-        {
-            var map = PerlinNoise.Generate(5, 5, seed);
-
-            map.ValueAt(x, y).Should().BeInRange(0, 1);
-        }
-
-        [Property(Arbitrary = new[] { typeof(DoubleGenerators.UnitIntervalUpperBoundExclusive) })]
-        public void GeneratesSameMapIfSeedUnchanged(int seed, double x, double y)
-        {
-            var map1 = PerlinNoise.Generate(5, 5, seed);
-            var map2 = PerlinNoise.Generate(5, 5, seed);
-
-            map1.ValueAt(x, y).Should().BeApproximately(map2.ValueAt(x, y), double.Epsilon);
-        }
+        protected override IProceduralTexture CreateProceduralTexture(int seed) => PerlinNoise.Generate(5, 5, seed);
     }
 }
--- Bearded.Utilities.Tests/Noise/UniformNoiseTests.cs
+++ Bearded.Utilities.Tests/Noise/UniformNoiseTests.cs
@@ -1,64 +1,9 @@
-using System;
 using Bearded.Utilities.Noise;
-using Bearded.Utilities.Tests.Generators;
-using FluentAssertions;
-using FsCheck.Xunit;
 
 namespace Bearded.Utilities.Tests.Noise
 {
-    public sealed class UniformNoiseTests
+    public sealed class UniformNoiseTests : NoiseTests
     {
-        [Property(Arbitrary = new[] { typeof(DoubleGenerators.UnitIntervalUpperBoundExclusive) })]
-        public void GeneratesMapThatThrowsIfXCoordinateTooSmall(int seed, double x, double y)
-        {
-            var map = UniformNoise.Generate(5, 5, seed);
-
-            Func<double> action = () => map.ValueAt(x - 1.0, y);
-            action.Should().Throw<ArgumentOutOfRangeException>();
-        }
-
-        [Property(Arbitrary = new[] { typeof(DoubleGenerators.UnitIntervalUpperBoundExclusive) })]
-        public void GeneratesMapThatThrowsIfXCoordinateTooBig(int seed, double x, double y)
-        {
-            var map = UniformNoise.Generate(5, 5, seed);
-
-            Func<double> action = () => map.ValueAt(x + 1.0, y);
-            action.Should().Throw<ArgumentOutOfRangeException>();
-        }
-
-        [Property(Arbitrary = new[] { typeof(DoubleGenerators.UnitIntervalUpperBoundExclusive) })]
-        public void GeneratesMapThatThrowsIfYCoordinateTooSmall(int seed, double x, double y)
-        {
-            var map = UniformNoise.Generate(5, 5, seed);
-
-            Func<double> action = () => map.ValueAt(x, y - 1.0);
-            action.Should().Throw<ArgumentOutOfRangeException>();
-        }
-
-        [Property(Arbitrary = new[] { typeof(DoubleGenerators.UnitIntervalUpperBoundExclusive) })]
-        public void GeneratesMapThatThrowsIfYCoordinateTooBig(int seed, double x, double y)
-        {
-            var map = UniformNoise.Generate(5, 5, seed);
-
-            Func<double> action = () => map.ValueAt(x, y + 1.0);
-            action.Should().Throw<ArgumentOutOfRangeException>();
-        }
-
-        [Property(Arbitrary = new[] { typeof(DoubleGenerators.UnitIntervalUpperBoundExclusive) })]
-        public void GeneratesMapWithValuesInUnitInterval(int seed, double x, double y)
-        {
-            var map = UniformNoise.Generate(5, 5, seed);
-
-            map.ValueAt(x, y).Should().BeInRange(0, 1);
-        }
-
-        [Property(Arbitrary = new[] { typeof(DoubleGenerators.UnitIntervalUpperBoundExclusive) })]
-        public void GeneratesSameMapIfSeedUnchanged(int seed, double x, double y)
-        {
-            var map1 = UniformNoise.Generate(5, 5, seed);
-            var map2 = UniformNoise.Generate(5, 5, seed);
-
-            map1.ValueAt(x, y).Should().BeApproximately(map2.ValueAt(x, y), double.Epsilon);
-        }
+        protected override IProceduralTexture CreateProceduralTexture(int seed) => UniformNoise.Generate(5, 5, seed);
     }
 }
